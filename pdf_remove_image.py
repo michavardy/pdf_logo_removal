7767e8e5c27ec250188a9a1e2e672b0219106ac9
micha <micha.vardy@tufin.com>
2023-03-30 16:00:05 +0300

    changes

--- pdf_remove_image.py
+++ pdf_remove_image.py
@@ -1,4 +1,42 @@
 import PyPDF2
+from PyPDF2 import PageObject
+from PyPDF2.generic import ByteStringObject, NameObject, DictionaryObject, RectangleObject
+from PIL import Image
+
+'''
+    Methods:
+        get_object()
+    attributes in page object
+        '/Annots': All links and widgets
+        '/Contents', 
+        '/CropBox', 
+        '/Group', 
+        '/MediaBox', 
+        '/Parent', 
+        '/Resources':
+            '/ExtGState', 
+            '/Font', 
+            '/ProcSet', 
+            '/XObject': Holds all pictures: '/Im0', '/Im1', ...
+                /BitsPerComponent', 
+                '/ColorSpace', 
+                '/Filter', 
+                '/Height', 
+                '/Interpolate', 
+                '/SMask', 
+                '/Subtype', 
+                '/Type', 
+                '/Width'
+        '/Rotate', 
+        '/StructParents', 
+        '/Tabs', 
+        '/Type', 
+        '/ArtBox', 
+        '/BleedBox', 
+        '/TrimBox'
+'''
+
+# read logo
 
 # Open the PDF file in read-binary mode
 with open("example.pdf", "rb") as pdf_file:
@@ -8,26 +46,43 @@ with open("example.pdf", "rb") as pdf_file:
     # Get the first page of the PDF
     page = pdf_reader.pages[0] 
 
-    # black list
-    black_list =[]
-    # Loop over each annotation
-    for index, annotation in enumerate(page['/Annots']):
+    # remove "/Im0"
+    del page['/Resources']['/XObject']['/Im0']
 
-        # Get the annotation object
-        annotation_object = annotation.get_object()
+    # open jpeg
+    with open('image.jpg', 'rb') as f:
+        img_data = f.read()
+    
+    # open iimage
+    img = Image.open('image.jpg')
 
-        # Check if the annotation is an image
-        if  annotation_object['/Subtype'] == '/Image':
-            # Remove the annotation from the page
-            black_list.append(index)
-    # remove first image from annotations
-    del page['/Annots'][black_list[0]]
-    # Decrease the annotation count
-    page['/Count'] -= 1
+    # Get the width and height of the image
+    width, height = img.size
+    
+    # create new image
+    # Create a new form XObject for the image
+    xobj = DictionaryObject()
+    xobj.update({
+        NameObject('/Type'): NameObject('/XObject'),
+        NameObject('/Subtype'): NameObject('/Image'),
+        NameObject('/Width'): width,
+        NameObject('/Height'): height,
+        NameObject('/BitsPerComponent'): 8,
+        NameObject('/ColorSpace'): NameObject('/DeviceRGB'),
+        NameObject('/Filter'): NameObject('/DCTDecode'),
+        NameObject('/Length'): len(img_data)
+    })
+    xobj.stream = ByteStringObject(img_data)
 
-    # Create a new PdfFileWriter object and add the modified page to it
-    pdf_writer = PyPDF2.PdfFileWriter()
-    pdf_writer.addPage(page)
+    # Add the form XObject to the new PDF page
+    page[NameObject('/Resources')][NameObject('/XObject')] = DictionaryObject()
+    page[NameObject('/Resources')][NameObject('/XObject')][NameObject('/Image')] = xobj
+    page[NameObject('/Contents')] = ByteStringObject(b'q\n1 0 0 1 0 0 cm\n/Image Do\nQ\n')
+
+        
+        # Create a new PdfFileWriter object and add the modified page to it
+    pdf_writer = PyPDF2.PdfWriter()
+    pdf_writer.add_page(page)
 
     # Create a new PDF file in write-binary mode and write the modified page to it
     with open("modified.pdf", "wb") as output_file:
